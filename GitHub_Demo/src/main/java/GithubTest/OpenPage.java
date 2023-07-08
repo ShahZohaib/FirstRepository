6f79745fae7913faf10300ab33fac5f70f457766
ShahZohaib <138885858+ShahZohaib@users.noreply.github.com>
2023-07-08 16:33:31 +0530

    New Class Created using OrangeHrm Website

---
+++ GitHub_Demo/src/main/java/GithubTest/OpenPage.java
@@ -0,0 +1,32 @@
+package GithubTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class OpenPage {
+	public static void main(String[] args) {
+		WebDriver d = new ChromeDriver();
+		d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		d.manage().window().maximize();
+		
+		By login =By.xpath("//input[@name='username']");
+		WebElement login_data = d.findElement(login);
+		login_data.click();
+		login_data.sendKeys("Admin");
+		
+		By pass = By.xpath("//input[@name='password']");
+		WebElement password = d.findElement(pass);
+		password.click();
+		password.sendKeys("admin123");
+		
+		By submit = By.xpath("//button[@type='submit']");
+		WebElement click_submit = d.findElement(submit);
+		click_submit.submit();
+		
+		d.quit();
+	}
+}
