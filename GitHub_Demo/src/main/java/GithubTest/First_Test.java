2a597c361c0bef3b6bdfdf5f9ef7625d52ee62ec
ShahZohaib <138885858+ShahZohaib@users.noreply.github.com>
2023-07-08 15:27:14 +0530

    Pushing the Updated Code

--- GitHub_Demo/src/main/java/GithubTest/First_Test.java
+++ GitHub_Demo/src/main/java/GithubTest/First_Test.java
@@ -1,13 +1,19 @@
 package GithubTest;
 
 public class First_Test {
-	public void test_Method1()
+	public static void test_Method1()
 	{
 		System.out.println("First Test Method1");
 	}
-	public void test_Method2()
+	public static void test_Method2()
 	{
 		System.out.println("First Test Method2");
+	}
+	public static void main(String[] args) {
+		int a=10,b=20;
+		System.out.println(a+b);
+		test_Method1();
+		test_Method2();
 	}
 	
 }
