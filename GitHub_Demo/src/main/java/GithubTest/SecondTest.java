a0e0a59019d1fe986ab07acf22f95acc6d97445e
ShahZohaib <138885858+ShahZohaib@users.noreply.github.com>
2023-07-08 11:41:53 +0530

    method 2 added and method1 updated

--- GitHub_Demo/src/main/java/GithubTest/SecondTest.java
+++ GitHub_Demo/src/main/java/GithubTest/SecondTest.java
@@ -3,6 +3,10 @@ package GithubTest;
 public class SecondTest {
 	public void test_Method1()
 	{
-		System.out.println("Second Test Method");
+		System.out.println("Second Test Method1");
+	}
+	public void test_Method2()
+	{
+		System.out.println("Second Test Method2");
 	}
 }
