6053d6680bbb52ac21291f8ece68bcc58bbdf8e5
ShahZohaib <138885858+ShahZohaib@users.noreply.github.com>
2023-07-08 15:39:23 +0530

    New Method Added

--- GitHub_Demo/src/main/java/GithubTest/First_Test.java
+++ GitHub_Demo/src/main/java/GithubTest/First_Test.java
@@ -9,11 +9,17 @@ public class First_Test {
 	{
 		System.out.println("First Test Method2");
 	}
+	public static void test_Method3()
+	{
+		System.out.println("First Test Method3");
+	}
 	public static void main(String[] args) {
 		int a=10,b=20;
 		System.out.println("Addition of Two Numbers: "+(a+b));
 		test_Method1();
 		test_Method2();
+		test_Method3();
+		
 	}
 	
 }
