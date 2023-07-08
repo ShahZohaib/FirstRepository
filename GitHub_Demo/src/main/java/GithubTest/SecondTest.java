e758d8e995f3d0e1799825aa9cc311a2f0db6fb0
ShahZohaib <138885858+ShahZohaib@users.noreply.github.com>
2023-07-08 15:59:35 +0530

    Using TestNG Methods added and updated

--- GitHub_Demo/src/main/java/GithubTest/SecondTest.java
+++ GitHub_Demo/src/main/java/GithubTest/SecondTest.java
@@ -1,12 +1,27 @@
 package GithubTest;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
+
 public class SecondTest {
+	@BeforeMethod
+	public void beforetest_method()
+	{
+		System.out.println("Before Method Second Test");
+	}	
+	@Test
 	public void test_Method1()
 	{
 		System.out.println("Second Test Method1");
 	}
+	@Test
 	public void test_Method2()
 	{
 		System.out.println("Second Test Method2");
+	}
+	@AfterMethod
+	public void aftetest_Method()
+	{
+		System.out.println("After Method Second Test");
 	}
 }
