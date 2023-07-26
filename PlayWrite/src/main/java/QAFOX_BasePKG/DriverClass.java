887a9eddcf0b7686b4c4c6ab54bf3d0bb156090d
ShahZohaib <138885858+ShahZohaib@users.noreply.github.com>
2023-07-26 14:59:51 +0530

    QAFOX Test case in Playwright

---
+++ PlayWrite/src/main/java/QAFOX_BasePKG/DriverClass.java
@@ -0,0 +1,18 @@
+package QAFOX_BasePKG;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class DriverClass 
+{
+	public static Page p;
+	public static void driver(String url)
+	{
+		Playwright pw = Playwright.create();
+		  Browser bw = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		  p = bw.newPage();
+		  p.navigate(url);
+	}
+}
